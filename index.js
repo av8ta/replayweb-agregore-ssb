f7384f048653d40df2d968f0c159dd950b7bd449
av8ta <av8ta@protonmail.com>
2022-06-05 18:03:26 +0800

    fix typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path'
 const moduleURL = new URL(import.meta.url)
 const __dirname = path.dirname(moduleURL.pathname)
 
-const sbot = await Server())
+const sbot = await Server()
 const blobId = await addBlob(sbot, path.join(__dirname, 'wacz', 'lisp.wacz'))
 
 console.log('wacz blob id is', blobId)
